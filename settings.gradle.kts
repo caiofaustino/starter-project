e8c461f4eef5095852da6faffbe8ad5f3be9ba3d
Caio Faustino <caiofaustino@gmail.com>
2022-04-18 15:12:30 +0200

    Organize versions using new Version Catalogs.
    Signed-off-by: Caio Faustino <caiofaustino@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,8 @@
 
+
+rootProject.name = "Starter Project"
+include(":app")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -9,11 +13,39 @@ pluginManagement {
 
 @kotlin.Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
+    // If this mode is set, any repository declared directly in a project,
+    // either directly or via a plugin, will trigger a build error.
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
     }
 }
-rootProject.name = "Starter Project"
-include(":app")
+
+@kotlin.Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            library("coreKtx", "androidx.core", "core-ktx").version("1.7.0")
+
+            version("compose", "1.0.1")
+            library("compose.ui", "androidx.compose.ui", "ui").versionRef("compose")
+            library("compose.ui.toolingPreview", "androidx.compose.ui", "ui-tooling-preview").versionRef("compose")
+            library("compose.material", "androidx.compose.material", "material").versionRef("compose")
+            bundle("compose", listOf("compose.ui", "compose.ui.toolingPreview", "compose.material"))
+            library("compose.ui.tooling", "androidx.compose.ui", "ui-tooling").versionRef("compose")
+
+            library("lifecycle.runtimeKtx", "androidx.lifecycle", "lifecycle-runtime-ktx").version("2.3.1")
+            library("activity.compose", "androidx.activity", "activity-compose").version("1.3.1")
+        }
+        create("testLibs") {
+            library("junit", "junit", "junit").version("4.13.2")
+        }
+        create("instrumentedTestLibs") {
+            library("junitExt", "androidx.test.ext", "junit").version("1.1.3")
+            library("espresso", "androidx.test.espresso", "espresso-core").version("3.4.0")
+            library("compose.junit4", "androidx.compose.ui", "ui-test-junit4").version("1.0.1")
+        }
+    }
+}
+
