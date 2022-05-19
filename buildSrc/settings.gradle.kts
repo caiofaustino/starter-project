f2d0b9fe4c510ca2c057725b1de9d712c1487d42
Caio Faustino <caiofaustino@gmail.com>
2022-05-19 23:01:13 +0200

    Update gitignore and make settings.gradle from buildSrc a kotlin file.
    Signed-off-by: Caio Faustino <caiofaustino@gmail.com>

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
     }
 }
 
+@kotlin.Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
