524358ea71339e386d05a5ea50383fc29a5da128
Caio Faustino <caiofaustino@gmail.com>
2024-05-21 01:09:03 +0200

    Fix apply Ktlint plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.app) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+
+    alias(libs.plugins.ktlint.gradle) apply true
     alias(libs.plugins.detekt) apply false
-    alias(libs.plugins.ktlint.gradle) apply false
+}
+
+ktlint {
+    version.set(libs.versions.ktlint.toString())
+    android.set(true)
 }
